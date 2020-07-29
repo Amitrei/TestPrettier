aaf5a81f6addd01bcbf8ca36eae63a49d212b9fc
Amit <amitreinich@gmail.com>
2020-07-30 02:59:59 +0300

    init commint

--- src/MainClass.java
+++ src/MainClass.java
@@ -9,7 +9,8 @@ public class MainClass {
         Table createTable = new Table(100);
         createTable.createHeaders("1234","124");
         createTable.createRow("123412355","12445655");
-        createTable.createRow("Coupon{id=0, company=3, title='BestCoupon', Category : FOOD, description='Description', start_date=2020-07-30, end_date=2020-08-09, amount=100, price=100.0, image='image.png'}","214");
+        createTable.createRow("Coupon{id=0, company=3, title='BestCoupon', Category : FOOD, description='Description', start_date=2020-07-30, end_date=2020-08-09, amount=100, price=100.0, image='image.png'}",
+                "Coupon{id=0, company=3, title='BestCoupon', Category : FOOD, description='Description', start_date=2020-07-30, end_date=2020-08-09");
         createTable.render();
 
 
